b6518c3b41ad5c51769bb7c4ff8b4d0a29d87572
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2011-05-31 20:10:47 +0200

    Adding words dictionary instance to main anagram class

--- src/main/java/fr/xebia/katas/anagram/Anagram.java
+++ src/main/java/fr/xebia/katas/anagram/Anagram.java
@@ -3,7 +3,13 @@ package fr.xebia.katas.anagram;
 import java.util.List;
 
 public class Anagram {
-    
+
+    private List<String> words;
+
+    public Anagram() {
+        this.words =  WordFileLoader.loadFileLines("words.txt");
+    }
+
     public List<String> anagram(String word) {
         return null;
     }
